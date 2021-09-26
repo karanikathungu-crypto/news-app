8a1b61abe6169044c6e7fdc80e0aeb32061594ce
karanikathungu-crypto <kathungudev@gmail.com>
2021-09-26 22:19:53 +0300

    Add getter and setter for User class

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -3,4 +3,16 @@ package models;
 public class User {
     private String name;
     private int id;
+
+    public User(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
