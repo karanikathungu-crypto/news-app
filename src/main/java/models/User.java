c8f8ab6938ecbc75421f74c36ddfe97a4b3d7f30
karanikathungu-crypto <kathungudev@gmail.com>
2021-09-26 23:03:29 +0300

    Add objects class for user class

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,18 +1,34 @@
 package models;
 
+import java.util.Objects;
+
 public class User {
-    private String name;
+    private String userName;
     private int id;
 
-    public User(String name){
-        this.name = name;
+    public User(String userName){
+        this.userName = userName;
     }
 
     public String getName() {
-        return name;
+        return userName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String userName) {
+        this.userName = userName;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Departments)) return false;
+        User user = (User) o;
+        return id == user.id &&
+                Objects.equals(userName, user.userName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userName , id);
     }
 }
+
