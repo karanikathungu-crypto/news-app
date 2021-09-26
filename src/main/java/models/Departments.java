815ccafac42d2dde30bd9d5a710b2545ee2415c5
karanikathungu-crypto <kathungudev@gmail.com>
2021-09-26 22:48:09 +0300

    Add objects class for departments class

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -1,11 +1,13 @@
 package models;
 
+import java.util.Objects;
+
 public class Departments {
     private String departmentName;
     private int staffNumber;
     private int id;
 
-    public Departments(String departmentName, int staffNumber){
+    public Departments(String departmentName, int staffNumber) {
         this.departmentName = departmentName;
         this.staffNumber = staffNumber;
     }
@@ -24,5 +26,20 @@ public class Departments {
 
     public void setStaffNumber(int staffNumber) {
         this.staffNumber = staffNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Departments)) return false;
+        Departments dept = (Departments) o;
+        return id == dept.id &&
+                Objects.equals(departmentName, dept.departmentName) &&
+                Objects.equals(staffNumber, dept.staffNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(departmentName, staffNumber, id);
     }
 }
