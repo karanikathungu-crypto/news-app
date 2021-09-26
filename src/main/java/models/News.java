a87f885025ec79c862366b499d31d7c304b12e67
karanikathungu-crypto <kathungudev@gmail.com>
2021-09-26 22:47:39 +0300

    Add the objects class for news class

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,8 +1,11 @@
 package models;
 
+import java.util.Objects;
+
 public class News {
     private String headline;
     private String departmentName;
+    private int id;
 
     public News(String headline){
         this.headline = headline;
@@ -15,4 +18,19 @@ public class News {
     public void setHeadline(String headline) {
         this.headline = headline;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Departments)) return false;
+        News news = (News) o;
+        return id == news.id &&
+                Objects.equals(headline, news.headline);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(departmentName, headline, id);
+    }
+}
 }
