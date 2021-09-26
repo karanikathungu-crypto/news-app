c42bf8bb891618c9ae3fbf02e02121e944dc1456
karanikathungu-crypto <kathungudev@gmail.com>
2021-09-26 21:24:31 +0300

    Add constructor for news

---
+++ src/main/java/models/Departments.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Departments {
+    private String departmentName;
+    private int staffNumber;
+    private int id;
+
+    public Departments(String name, int staffNumber){
+
+    }
+}
---
+++ src/main/java/models/News.java
@@ -0,0 +1,10 @@
+package models;
+
+public class News {
+    private String headline;
+    private String departmentName;
+
+    public News(String headline){
+        this.headline = headline;
+    }
+}
