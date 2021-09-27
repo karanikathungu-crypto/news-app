bf7ca20adbb0aa67c716b976282c20313036473f
karanikathungu-crypto <kathungudev@gmail.com>
2021-09-27 11:27:01 +0300

    Add tables for departments and news

---
+++ src/main/resources/db/create.sql
@@ -0,0 +1,15 @@
+SET MODE PostgresSQL;
+CREATE DATABASE companynews;
+\c companynews;
+CREATE TABLE if NOT EXISTS news(
+    id SERIAL PRIMARY KEY,
+    headline VARCHAR,
+    information VARCHAR,
+);
+
+CREATE TABLE if NOT EXISTS departments(
+    id SERIAL PRIMARY KEY,
+    departmentName VARCHAR,
+    staffNumber INTEGER
+);
+
