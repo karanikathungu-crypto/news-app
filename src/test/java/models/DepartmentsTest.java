b1c1d2256ef4e92b20b2f936de8a287ff26e3414
karanikathungu-crypto <kathungudev@gmail.com>
2021-09-30 11:27:08 +0300

    Add tests for department class

--- src/test/java/models/DepartmentsTest.java
+++ src/test/java/models/DepartmentsTest.java
@@ -1,4 +1,37 @@
 package models;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class DepartmentsTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getDepartmentNameReturnsCorrectName() {
+        Departments testDepartment = setUpDepartments();
+        assertEquals("IT",testDepartment.getDepartmentName());
+
+    }
+
+    @Test
+    public void getStaffNumberReturnsCorrectNumber() {
+        Departments testDepartment = setUpDepartments();
+        assertEquals(10, testDepartment.getStaffNumber());
+    }
+
+
+    public Departments setUpDepartments(){
+        return new Departments("IT",10);
+    }
 }
